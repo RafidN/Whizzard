d3565bba21628d1140c93cb61031f9524b44f66f
RafidN <rafidn@outlook.com>
2024-02-18 08:07:17 -0600

    complete.

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,53 +1,44 @@
 import React, { useState } from "react";
 import "./index.css";
-import logo from "../src/CroppedLogo.png"
-
-
-
+import logo from "../src/CroppedLogo.png";
+import placehold from "../src/placeholder.jpg";
+import steth from "../src/Steth.png";
+import manimg from "../src/Man.png";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [analysisResult, setAnalysisResult] = useState(null);
 
-
-
-
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-  setSelectedFile(file);
+    setSelectedFile(file);
 
+    // Load image
+    const reader = new FileReader();
 
-  // Load image
-  const reader = new FileReader();
+    reader.onload = function (e) {
+      const uploadedImage = document.getElementById("uploadedImage");
+      if (uploadedImage) {
+        console.log("Image loaded successfully:", e.target.result);
+        uploadedImage.src = e.target.result;
+      } else {
+        console.error("Error: uploadedImage element not found");
+      }
+    };
 
-
-  reader.onload = function (e) {
-    const uploadedImage = document.getElementById("uploadedImage");
-    if (uploadedImage) {
-      console.log("Image loaded successfully:", e.target.result);
-      uploadedImage.src = e.target.result;
-    } else {
-      console.error("Error: uploadedImage element not found");
-    }
+    reader.readAsDataURL(file);
   };
-
-
-  reader.readAsDataURL(file);
- };
-
 
   const handleAnalysis = async () => {
     if (selectedFile) {
       const formData = new FormData();
       formData.append("image", selectedFile);
 
-
       try {
         const response = await fetch("http://127.0.0.1:5000/analyze", {
           method: "POST",
           body: formData,
         });
-
 
         if (response.ok) {
           const result = await response.json();
@@ -61,53 +52,68 @@ function App() {
     }
   };
 
-
   return (
     <>
-      <nav>
-        <figure className="nav__img--wrapper">
-          <img src={logo} alt="" className="nav__img" />
-        </figure>
-        <h1 className="nav__header">Whizzard</h1>
-      </nav>
-
-
       <div id="landing-page">
-        <div className="fullwidth">
-          <div className="left">
-            <h2 className="left__header2">Whizzard Analysis</h2>
-            <input
-              className="inputField"
-              type="file"
-              accept="image/*"
-              onChange={handleFileChange}
-            />
-            <div id="imageContainer">
-              <img src="" alt="" id="uploadedImage" />
+        <div className="row">
+          <nav>
+            <div className="nav__content">
+              <figure className="nav__img--wrapper">
+                <img src={logo} alt="" className="nav__img" />
+              </figure>
+              <h1 className="nav__header">WHIZZARD</h1>
             </div>
-            <button className="btn" onClick={handleAnalysis}>
-              Analyze
-            </button>
-          </div>
-          {analysisResult && (
-            <div className="right lp__right--results">
-              <div className="border-around">
-                <h2>Analysis Results:</h2>
-                <p>{analysisResult.status}</p>
+          </nav>
+
+          <div className="fullwidth">
+            <div className="top">
+              <input
+                className="inputField"
+                type="file"
+                accept="image/*"
+                onChange={handleFileChange}
+              />
+              <div id="imageContainer">
+                <img src={placehold} alt="" id="uploadedImage" />
               </div>
+              <button className="btn" onClick={handleAnalysis}>
+                <img className="btn__img" src={steth} alt="" />
+                <p className="btn__text">Generate Analysis</p>
+                <img className="btn__img" src={manimg} alt="" />
+              </button>
             </div>
-          )}
+            <div className="bottom">
+              {analysisResult && (
+                <>
+                  <div className="border-around">
+                    <h2 className="result__header">Analysis Results:</h2>
+                    {analysisResult.status &&
+                      analysisResult.status.split("|").map((part, index) => (
+                        <React.Fragment key={index}>
+                          {index > 0 && <br />}{" "}
+                          {/* Add <br> after the first part */}
+                          <p className="result__text">{part}</p>
+                        </React.Fragment>
+                      ))}
+                  </div>
+                  <button className="btn">
+                    <a
+                      href="https://4yk7hw-5173.csb.app/"
+                      className="btn__text"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Get AI Help
+                    </a>
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
         </div>
-
-
       </div>
     </>
   );
 }
 
-
 export default App;
-
-
-
-
