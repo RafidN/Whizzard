1d1542c7aced0adc4ad474e9c9fb81a763e9b9f3
RafidN <rafidn@outlook.com>
2024-02-17 18:17:16 -0600

    brightened hsv-> complete

--- flask-backend/backendFlask.py
+++ flask-backend/backendFlask.py
@@ -8,7 +8,7 @@ import colorsys
 app = Flask(__name__)
 CORS(app)  # This is crucial for cross-origin requests from your React app
 
-def find_predominant_color(image):
+def find_predominant_color_rgb(image):
     h, w, _ = image.shape
     center_x, center_y = w // 2, h // 2
     size = min(w, h) // 8
@@ -18,8 +18,7 @@ def find_predominant_color(image):
     predominant_color = kmeans.cluster_centers_.astype(int)[0]
     return tuple(predominant_color)
 
-def analyze_urine_color(color):
-    color_rgb = color[::-1]
+def analyze_urine_color(color_rgb):
     if color_rgb[0] > 200 and color_rgb[1] > 200 and color_rgb[2] > 160:
         return "Healthy (Hydrated)"
     elif color_rgb[0] > 160 and color_rgb[1] > 160 and color_rgb[2] < 100:
@@ -29,48 +28,60 @@ def analyze_urine_color(color):
     else:
         return "Consult a doctor for detailed analysis"
     
-def adjustImg(img, value = 30):
-    ## CLAHE ADJUSTMENT
-    # Convert the image from BGR to LAB color space
-    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    l_channel, a, b = cv2.split(lab)
+def rgb_to_hsv(r, g, b):
+    r, g, b = r/255.0, g/255.0, b/255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    df = mx-mn
+    if mx == mn:
+        h = 0
+    elif mx == r:
+        h = (60 * ((g-b)/df) + 360) % 360
+    elif mx == g:
+        h = (60 * ((b-r)/df) + 120) % 360
+    elif mx == b:
+        h = (60 * ((r-g)/df) + 240) % 360
+    if mx == 0:
+        s = 0
+    else:
+        s = (df/mx)*100
+    v = mx*100
+    return h, s, v
+
+def add_thirty_value(color_hsv):
+    # Convert the predominant color to a list for modification
+    hsv_list = list(color_hsv)
+
+    # Modify the values as needed
+    # For example, let's assume you want to increase the red component by 10
+    if(hsv_list[2] <= 70):
+         hsv_list[2] += 30
+    else:
+        hsv_list[2] = 100
+        
+    # Convert each value to an integer
+    hsv_list = [int(value) for value in hsv_list]
     
-    # Apply CLAHE to L-channel
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    c_l_channel = clahe.apply(l_channel)
     
-    # Merge the CLAHE enhanced L-channel with the original a and b channel
-    lab = cv2.merge((c_l_channel, a, b))
+    # Convert the list back to a tuple if necessary
+    hsv_tuple = tuple(hsv_list)
+    return hsv_tuple
     
-    # Convert the LAB image back to BGR
-    corrected_image = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
-
-    ## BRIGHTNESS ADJUSTMENT
-    hsv = cv2. cvtColor(corrected_image, cv2.COLOR_BGR2HSV)
-    h,s,v = cv2.split(hsv)
-
-    lim = 255 - cv2.split(hsv)
-    v[v > lim] = 255
-    v[v <= lim] += value
-
-    final_hsv = cv2.merge((h, s, v))
-    finalImg = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-
-    return finalImg
 
 def analyze_image(img):
-    imgStd = adjustImg(img)
-    predominant_color = find_predominant_color(imgStd)
-    health_status = analyze_urine_color(predominant_color)
+    predominant_color = find_predominant_color_rgb(img)
 
     # Convert numpy types to regular Python types
     predominant_color = tuple(map(int, predominant_color))
+    
     color_rgb = predominant_color[::-1]
     
-    
+    color_hsv = rgb_to_hsv(color_rgb[0], color_rgb[1], color_rgb[2])
+    brightened_hsv = add_thirty_value(color_hsv)
+    health_status = analyze_urine_color(color_rgb)
     health_status = str(health_status)
 
-    return {"status": health_status, "predominant_color": color_rgb}
+    return {"status": health_status, "predominant_color_rgb": color_rgb, "brightened_color_hsv": brightened_hsv}
 
 
 @app.route('/analyze', methods=['POST'])
@@ -78,9 +89,9 @@ def analyze_urine():
     file = request.files['image'].read()
     npimg = np.fromstring(file, np.uint8)
     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    
+
     analysis_result = analyze_image(img)
-    
+
     return jsonify(analysis_result)
 
 if __name__ == "__main__":
