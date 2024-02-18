5823e171e4c77f55632b2dee8afb1dbff1110d88
RafidN <rafidn@outlook.com>
2024-02-17 21:14:25 -0600

    react overhaul

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [analysisResult, setAnalysisResult] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleAnalysis = async () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('image', selectedFile);
+
+      try {
+        const response = await fetch('http://127.0.0.1:5000/analyze', {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          setAnalysisResult(result);
+        } else {
+          console.error('Error analyzing urine:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error analyzing urine:', error.message);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Urine Analysis App</h1>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      <button onClick={handleAnalysis}>Analyze</button>
+
+      {analysisResult && (
+        <div>
+          <h2>Analysis Result</h2>
+          <p>Status: {analysisResult.status}</p>
+          <p>Predominant Color (RGB): {analysisResult.predominant_color_rgb.join(', ')}</p>
+          <p>Brightened Color (HSV): {analysisResult.brightened_color_hsv.join(', ')}</p>
+        </div>
+      )}
     </div>
   );
 }
